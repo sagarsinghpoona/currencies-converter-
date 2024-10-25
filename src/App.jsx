885d1e294a6d5063b2dfee675b5210c9951a1201
sagarsinghpoona <112770216+sagarsinghpoona@users.noreply.github.com>
2024-10-25 16:43:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CurrencyConvertor from "./components/currency-convertor";
+
+function App() {
+  return (
+    <div className="  items-center justify-center">
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
